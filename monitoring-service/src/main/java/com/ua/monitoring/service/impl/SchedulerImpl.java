f1ee2008147c1766c2b684a9793eed8bd067f646
alex <->
2022-05-27 17:39:50 +0300

    scheduler migrated to vertx

--- monitoring-service/src/main/java/com/ua/monitoring/service/impl/SchedulerImpl.java
+++ monitoring-service/src/main/java/com/ua/monitoring/service/impl/SchedulerImpl.java
@@ -4,58 +4,36 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.ZoneId;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ScheduledFuture;
 
 import com.ua.monitoring.dto.MonitoringResultDto;
 import com.ua.monitoring.model.MonitoredEndpoint;
 import com.ua.monitoring.service.MonitoredEndpointService;
 import com.ua.monitoring.service.Scheduler;
-import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import io.vertx.core.AbstractVerticle;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
-public class SchedulerImpl implements SchedulingConfigurer, Scheduler {
-
-    private final ScheduledTaskRegistrar scheduledTaskRegistrar;
-    private final ThreadPoolTaskScheduler taskScheduler;
+public class SchedulerImpl extends AbstractVerticle implements Scheduler {
     private final WebClient webClient;
     private final MonitoredEndpointService monitoredEndpointService;
-
-    private final Map<Long, ScheduledFuture<?>> futureMap = new HashMap<>();
+    private final Map<Long, Long> endpointIdPeriodicIdMap = new HashMap<>();
 
     public SchedulerImpl(
-            final ScheduledTaskRegistrar scheduledTaskRegistrar,
-            final ThreadPoolTaskScheduler taskScheduler,
             final WebClient webClient,
             final MonitoredEndpointService monitoredEndpointService
     ) {
-        this.scheduledTaskRegistrar = scheduledTaskRegistrar;
-        this.taskScheduler = taskScheduler;
         this.webClient = webClient;
         this.monitoredEndpointService = monitoredEndpointService;
     }
 
     @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        if (taskRegistrar.getScheduler() == null) {
-            taskRegistrar.setScheduler(taskScheduler);
-        }
-    }
-
-    @Override
-    public void addMonitoringJob(MonitoredEndpoint monitoredEndpoint) {
-        if (futureMap.containsKey(monitoredEndpoint.getId())) {
-            return;
+    public void addMonitoringJob(final MonitoredEndpoint monitoredEndpoint) {
+        if (endpointIdPeriodicIdMap.containsKey(monitoredEndpoint.getId())) {
+            vertx.cancelTimer(endpointIdPeriodicIdMap.get(monitoredEndpoint.getId()));
         }
 
         if (monitoredEndpoint.getDateOfLastCheck() == null
@@ -64,33 +42,19 @@ public class SchedulerImpl implements SchedulingConfigurer, Scheduler {
                         monitoredEndpoint.getMonitoredInterval()))) {
             createMonitoredResult(monitoredEndpoint);
         }
-
-        ScheduledFuture<?> future = scheduledTaskRegistrar.getScheduler().schedule(
-                () -> createMonitoredResult(monitoredEndpoint),
-                t -> {
-                    Calendar nextExecutionTime = new GregorianCalendar();
-
-                    nextExecutionTime.setTime(Date.from(
-                            monitoredEndpoint.getDateOfLastCheck().atZone(ZoneId.systemDefault()).toInstant()));
-                    nextExecutionTime.add(Calendar.SECOND, monitoredEndpoint.getMonitoredInterval());
-                    return nextExecutionTime.getTime();
-                });
-
-        configureTasks(scheduledTaskRegistrar);
-        futureMap.put(monitoredEndpoint.getId(), future);
     }
 
     @Override
     public void removeMonitoringJob(final Long monitoredEndpointId) {
-        if (!futureMap.containsKey(monitoredEndpointId)) {
+        if (!endpointIdPeriodicIdMap.containsKey(monitoredEndpointId)) {
             return;
         }
-        ScheduledFuture<?> future = futureMap.get(monitoredEndpointId);
-        future.cancel(true);
-        futureMap.remove(monitoredEndpointId);
+
+        vertx.cancelTimer(endpointIdPeriodicIdMap.get(monitoredEndpointId));
+        endpointIdPeriodicIdMap.remove(monitoredEndpointId);
     }
 
-    private void createMonitoredResult(MonitoredEndpoint monitoredEndpoint) {
+    private void createMonitoredResult(final MonitoredEndpoint monitoredEndpoint) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(monitoredEndpoint.getUrl()))
@@ -105,6 +69,14 @@ public class SchedulerImpl implements SchedulingConfigurer, Scheduler {
         }
 
         monitoredEndpointService.updateLastCheck(monitoredEndpoint);
+
+        schedule(monitoredEndpoint);
+    }
+
+    private void schedule(final MonitoredEndpoint monitoredEndpoint) {
+        endpointIdPeriodicIdMap.put(monitoredEndpoint.getId(),
+                vertx.setTimer(monitoredEndpoint.getMonitoredInterval() * 1000,
+                e -> createMonitoredResult(monitoredEndpoint)));
     }
 
     private void createResult(final MonitoringResultDto monitoringResultDto) {
